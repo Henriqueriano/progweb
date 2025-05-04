c0d38217c1e864aa422c6b4357fb2e4ebbc8fd69
Daniel H <daniel.neves0021@gmail.com>
2025-05-04 20:00:59 -0300

    bug: falta muita coisa ainda

--- seed-project-cli/frontend/src/app/auth/authentication.component.ts
+++ seed-project-cli/frontend/src/app/auth/authentication.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
   {
   selector: 'app-authnetication',
   standalone: true,
-  template: '<h1> Componente de Autenticação </h1>'
+  template: `
+    <h1>Componente de Autenticação</h1>
+    <header class="row spacing">
+      <nav class="col-md-8 col-md-offset-2">
+        <ul class="nav nav-tabs">
+          <li><a > SigUp</a></li>
+          <li><a > SigIn</a></li>
+          <li><a > LogOut</a></li>
+        </ul>
+      </nav>
+    </header>
+  `
   })
 export class AuthenticationComponent
 {
