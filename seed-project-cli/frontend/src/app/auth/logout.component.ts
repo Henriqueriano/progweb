20e9393d01f3b074d95a585e7a97b17fb4936771
daniel.neves0021 <daniel.neves0021@gmail.com>
2025-05-06 17:08:55 +0000

    feat: projeto ok para inicio das alteracoes do trabalho

--- seed-project-cli/frontend/src/app/auth/logout.component.ts
+++ seed-project-cli/frontend/src/app/auth/logout.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
   selector: 'app-logout',
   standalone: true,
   template: `
+  <br>
   <div class="col-md-8 col-md-offset-2">
   <button class="btn btn-danger" (click)="onLogout()">Logout</button>
   </div>`
--- seed-project-cli/frontend/src/app/auth/signin.component.ts
+++ seed-project-cli/frontend/src/app/auth/signin.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector:'app-signin',
   standalone: true,
+  imports: [ ReactiveFormsModule ],
   templateUrl: './signin.component.html'
 })
 
-export class SigninComponent {}
+export class SigninComponent implements OnInit 
+{
+  myFormIn! : FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.myFormIn = this.fb.group({
+      emailTS: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      passwordTS: [null, Validators.compose([ Validators.required, Validators.minLength(4), this.minusculoFValidator])]
+    });
+  }
+  onSubmit() {
+    console.log(this.myFormIn.value);
+  }
+  minusculoFValidator(control: AbstractControl) {
+   const pass = control.value as string;
+
+   if ((pass !== pass?.toLowerCase()) && (pass !== null)) {
+    return { minusculoF: true };
+  }
+  return null;
+}
+}
